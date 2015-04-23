b96a37423117037f18c7eada69b59b51aa30122d
metal.flow@gmail.com <metal.flow@gmail.com>
2015-04-22 23:20:15 -0600

    LAST GODDAM JAVADAOCS I EVER DO!

--- app/src/main/java/com/intercom/video/twoway/Streaming/VideoStreaming.java
+++ app/src/main/java/com/intercom/video/twoway/Streaming/VideoStreaming.java
@@ -3,6 +3,7 @@ package com.intercom.video.twoway.Streaming;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.util.Log;
 import android.widget.ImageView;
 
 import com.intercom.video.twoway.Utilities.Utilities;
@@ -15,15 +16,14 @@ import java.util.Arrays;
 
 /**
  * @author Sean Luther
- * This class contains things that deal with transmitting and receiving video / audio streams
+ *         This class contains things that deal with transmitting and receiving video / audio streams
  */
-public class VideoStreaming
-{
+public class VideoStreaming {
     private int LISTENING_SERVICE_PORT = 2049;
     private Utilities utilities;
     Socket tcpSocket; //Used when we are the client
     ServerSocket tcpServerSocket; //Used for accepting connections when we are the server
-    // Lower level streams, good for transfering raw bytes of video data
+    // Lower level streams, good for transferring raw bytes of video data
     InputStream tcpIn;
     OutputStream tcpOut;
 
@@ -46,54 +46,42 @@ public class VideoStreaming
      * @param a
      * @param utilities
      */
-    public VideoStreaming(Audio a, Utilities utilities)
-    {
+    public VideoStreaming(Audio a, Utilities utilities) {
         this.utilities = utilities;
         connected = false;
-        audioEngine=a;
+        audioEngine = a;
     }
 
-    public int getLISTENING_SERVICE_PORT()
-    {
+    public int getLISTENING_SERVICE_PORT() {
         return LISTENING_SERVICE_PORT;
     }
 
-    public void setLISTENING_SERVICE_PORT(int LISTENING_SERVICE_PORT)
-    {
+    public void setLISTENING_SERVICE_PORT(int LISTENING_SERVICE_PORT) {
         this.LISTENING_SERVICE_PORT = LISTENING_SERVICE_PORT;
     }
 
     /**
      * Close the streams and socket
      */
-    public void closeConnection()
-    {
-        try
-        {
+    public void closeConnection() {
+        try {
             tcpIn.close();
-        } catch (Exception e)
-        {
+        } catch (Exception e) {
 
         }
-        try
-        {
+        try {
             tcpOut.close();
-        } catch (Exception e)
-        {
+        } catch (Exception e) {
 
         }
-        try
-        {
+        try {
             tcpSocket.close();
-        } catch (Exception e)
-        {
+        } catch (Exception e) {
 
         }
-        try
-        {
+        try {
             tcpServerSocket.close();
-        } catch (Exception e)
-        {
+        } catch (Exception e) {
 
         }
 
@@ -102,17 +90,14 @@ public class VideoStreaming
 
     /**
      * Listen for a connection.  This should only be called from a seperate thread so the main
-     * thread isnt blocked
+     * thread isn't blocked
+     *
      * @param jpegImageView
      */
-    public void listenForMJpegConnection(final ImageView jpegImageView)
-    {
-        Thread listenForConnectionThread = new Thread()
-        {
-            public void run()
-            {
-                try
-                {
+    public void listenForMJpegConnection(final ImageView jpegImageView) {
+        Thread listenForConnectionThread = new Thread() {
+            public void run() {
+                try {
                     System.out.println("Listening for MJpeg connection!");
 
                     closeConnection();
@@ -125,15 +110,14 @@ public class VideoStreaming
                     // if we got here with no exception we can assume we are connected
                     connected = true;
 
-                    System.out.println("Connection has been established!  Now entering image capture loop, yay!");
+                    Log.i("VideoStreaming", "Connection has been established!  Now entering image capture loop, yay!");
                     byte[] imageSizeData = new byte[4];
                     byte[] audioSizeData = new byte[4];
 
                     long imageSize;
                     long audioSize;
 
-                    while (connected)
-                    {
+                    while (connected) {
 
                         // read in an integer (4 bytes) of data to determine the size of the jpeg we are about to receive
                         tcpIn.read(imageSizeData, 0, 4);
@@ -154,8 +138,7 @@ public class VideoStreaming
                         // read the jpeg portion of the message
                         // keep reading the stream until all bytes of this image have been read
                         // reads in chunks of 1024 bytes at a time
-                        while (totalJpegBytesReceived < imageSize)
-                        {
+                        while (totalJpegBytesReceived < imageSize) {
                             if (imageSize - totalJpegBytesReceived > 1024)
                                 totalJpegBytesReceived += tcpIn.read(receivedJpegByteArray, totalJpegBytesReceived, 1024);
                             else
@@ -166,8 +149,7 @@ public class VideoStreaming
                         // read the audio portion of the message
                         // keep reading the stream until all bytes of this audio have been read
                         // reads in chunks of 1024 bytes at a time
-                        while (totalAudioBytesReceived < audioSize)
-                        {
+                        while (totalAudioBytesReceived < audioSize) {
                             if (audioSize - totalAudioBytesReceived > 1024)
                                 totalAudioBytesReceived += tcpIn.read(receivedAudioByteArray, totalAudioBytesReceived, 1024);
                             else
@@ -178,9 +160,7 @@ public class VideoStreaming
                         decodeFrameAndAudioAsynch(totalJpegBytesReceived, receivedJpegByteArray, totalAudioBytesReceived, receivedAudioByteArray, jpegImageView);
                     }
 
-                }
-                catch (Exception e)
-                {
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
@@ -190,57 +170,45 @@ public class VideoStreaming
     }
 
     /**
-     asynchronously decode a frame of video and audio so that we don't cause
-     tcp stack to get backed up with incoming tcp data during the decode process
-     @param jpegLength length of the jpeg data being passed in
-     @param jpegData the actual jpeg data
-     @param audioLength the length of the audio data being passed in
-     @param audioData the actual raw pcm audio data
-     @param jpegImageView the image view we are displaying the jpeg to
+     * asynchronously decode a frame of video and audio so that we don't cause
+     * tcp stack to get backed up with incoming tcp data during the decode process
+     *
+     * @param jpegLength    length of the jpeg data being passed in
+     * @param jpegData      the actual jpeg data
+     * @param audioLength   the length of the audio data being passed in
+     * @param audioData     the actual raw pcm audio data
+     * @param jpegImageView the image view we are displaying the jpeg to
      */
-    public void decodeFrameAndAudioAsynch(final int jpegLength, final byte[] jpegData, final int audioLength, final byte[] audioData, final ImageView jpegImageView)
-    {
-        Thread decodeFrameThread = new Thread()
-        {
-            public void run()
-            {
+    public void decodeFrameAndAudioAsynch(final int jpegLength, final byte[] jpegData, final int audioLength, final byte[] audioData, final ImageView jpegImageView) {
+        Thread decodeFrameThread = new Thread() {
+            public void run() {
                 // dont do anything if it is busy decoding a frame
                 // this is not a synchronized section because synchronized sections
                 // would start building up waiting for others to finish
-                if (!currentlyDecodingFrame)
-                {
+                if (!currentlyDecodingFrame) {
                     currentlyDecodingFrame = true;
-                    try
-                    {
+                    try {
                         audioEngine.playAudioChunk(Arrays.copyOf(audioData, audioLength));
-                    } catch (Exception e)
-                    {
+                    } catch (Exception e) {
 
                     }
-                    try
-                    {
+                    try {
                         receivedBitmap = BitmapFactory.decodeByteArray(jpegData, 0, jpegLength);
-                    } catch (Exception e)
-                    {
+                    } catch (Exception e) {
                         e.printStackTrace();
                     }
-                    ((Activity) (utilities.mainContext)).runOnUiThread(new Runnable()
-                    {
+                    ((Activity) (utilities.mainContext)).runOnUiThread(new Runnable() {
                         @Override
-                        public void run()
-                        {
-                            try
-                            {
+                        public void run() {
+                            try {
                                 jpegImageView.setImageBitmap(receivedBitmap);
-                            }
-                            catch (Exception e)
-                            {
+                            } catch (Exception e) {
 
                             }
                         }
                     });
 
-                    currentlyDecodingFrame=false;
+                    currentlyDecodingFrame = false;
                 }
             }
         };
@@ -250,24 +218,24 @@ public class VideoStreaming
 
     /**
      * packs 4 bytes into a long
+     *
      * @param b4
      * @param b3
      * @param b2
      * @param b1
      * @return
      */
-    long packBytes(int b4, int b3, int b2, int b1)
-    {
+    long packBytes(int b4, int b3, int b2, int b1) {
         return ((0xFFL & b4) << 24) | ((0xFFL & b3) << 16) | ((0xFFL & b2) << 8) | (0xFFL & b1);
     }
 
     /**
      * unpack first 4 bytes of a long into a byte array
+     *
      * @param value
      * @return
      */
-    byte[] unPackBytes(long value)
-    {
+    byte[] unPackBytes(long value) {
         byte bytes[] = new byte[4];
 
         bytes[3] = (byte) ((value & 0xFF000000) >> 24);
@@ -280,18 +248,17 @@ public class VideoStreaming
 
     /**
      * Combines two byte arrays together
+     *
      * @param a1
      * @param a2
      * @return
      */
-    byte[] combineByteArrays(byte a1[], byte[] a2)
-    {
-        byte dataCombined[] = new byte[a1.length + a2.length ];
+    byte[] combineByteArrays(byte a1[], byte[] a2) {
+        byte dataCombined[] = new byte[a1.length + a2.length];
         // concatenate both arrays for sending
         for (int i = 0; i < a1.length; i++)
             dataCombined[i] = a1[i];
-        for (int i = a1.length; i < a1.length + a2.length; i++)
-        {
+        for (int i = a1.length; i < a1.length + a2.length; i++) {
             dataCombined[i] = a2[i - a1.length];
         }
 
@@ -300,34 +267,30 @@ public class VideoStreaming
 
     /**
      * Sends both the Audio and JPEG arrays out to the other device
+     *
      * @param jpegDataByteArray
      * @param audioDataByteArray
      */
-    void sendJpegFrame(final byte[] jpegDataByteArray, final byte[] audioDataByteArray)
-    {
+    void sendJpegFrame(final byte[] jpegDataByteArray, final byte[] audioDataByteArray) {
 
-        new Thread(new Runnable()
-        {
-            public void run()
-            {
-                    int jpegDataLength = jpegDataByteArray.length;
-                    int audioDataLength = audioDataByteArray.length;
-                    byte jpegDataLengthBytes[] = unPackBytes(jpegDataLength);
-                    byte audioDataLengthBytes[] = unPackBytes(audioDataLength);
+        new Thread(new Runnable() {
+            public void run() {
+                int jpegDataLength = jpegDataByteArray.length;
+                int audioDataLength = audioDataByteArray.length;
+                byte jpegDataLengthBytes[] = unPackBytes(jpegDataLength);
+                byte audioDataLengthBytes[] = unPackBytes(audioDataLength);
 
-                    byte packetSizePrefixBytes[] = combineByteArrays(jpegDataLengthBytes, audioDataLengthBytes);
+                byte packetSizePrefixBytes[] = combineByteArrays(jpegDataLengthBytes, audioDataLengthBytes);
 
-                    byte audioAndJpegDataCombined[] = combineByteArrays(jpegDataByteArray, audioDataByteArray);
-                    byte dataToSend[] = combineByteArrays(packetSizePrefixBytes, audioAndJpegDataCombined);
+                byte audioAndJpegDataCombined[] = combineByteArrays(jpegDataByteArray, audioDataByteArray);
+                byte dataToSend[] = combineByteArrays(packetSizePrefixBytes, audioAndJpegDataCombined);
 
-                try
-                    {
-                        tcpOut.write(dataToSend);
-                        tcpOut.flush();
-                    } catch (Exception e)
-                    {
-                        e.printStackTrace();
-                    }
+                try {
+                    tcpOut.write(dataToSend);
+                    tcpOut.flush();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         }).start();
 
@@ -335,19 +298,16 @@ public class VideoStreaming
 
     /**
      * Try to connect to the other device, this must be done before we send any Jpeg frames
+     *
      * @param ipAddress
      */
-    public void connectToDevice(final String ipAddress)
-    {
-        Thread openConnectionThread = new Thread()
-        {
-            public void run()
-            {
-                try
-                {
+    public void connectToDevice(final String ipAddress) {
+        Thread openConnectionThread = new Thread() {
+            public void run() {
+                try {
                     closeConnection();
 
-                    System.out.println("Trying to connect to remote device for streaming");
+                    Log.i("VideoStreaming", "Trying to connect to remote device for streaming");
                     tcpSocket = new Socket(ipAddress, getLISTENING_SERVICE_PORT());
                     tcpIn = tcpSocket.getInputStream();
                     tcpOut = tcpSocket.getOutputStream();
@@ -355,9 +315,8 @@ public class VideoStreaming
                     // if we got here with no exception we can assume we are connected
                     connected = true;
 
-                    System.out.println("Connected to remote device for streaming!");
-                } catch (Exception e)
-                {
+                    Log.i("VideoStreaming", "Connected to remote device for streaming!");
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
@@ -369,10 +328,10 @@ public class VideoStreaming
 
     /**
      * Returns the ip address of the remote device we are connected to
+     *
      * @return
      */
-    String getRemoteIpAddress()
-    {
+    String getRemoteIpAddress() {
 
         return tcpSocket.getRemoteSocketAddress().toString();
     }
--- app/src/main/java/com/intercom/video/twoway/Utilities/MyReceiver.java
+++ app/src/main/java/com/intercom/video/twoway/Utilities/MyReceiver.java
@@ -7,16 +7,14 @@ import android.content.Intent;
 import com.intercom.video.twoway.Services.ListenerService;
 
 /**
- * Created by seanluther on 4/12/15.
+ * @Author Sean luther on 4/12/15.
+ * A helper class for receiving intents
  */
-public class MyReceiver extends BroadcastReceiver
-{
+public class MyReceiver extends BroadcastReceiver {
 
     @Override
-    public void onReceive(Context context, Intent intent)
-    {
+    public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
-
             Intent service = new Intent(context, ListenerService.class);
             context.startService(service);
         }
